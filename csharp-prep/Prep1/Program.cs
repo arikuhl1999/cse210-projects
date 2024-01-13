9e91c5526b1de3a3eff722be84562011071c8d6e
arikuhl1999 <arielakuhl1999@byui.edu>
2024-01-12 20:46:32 -0700

    added firstname and lastname question for James Bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string lastname = Console.ReadLine();
+        
+        
+        System.Console.WriteLine("Your name is "+ lastname + ", " + firstname + " " + lastname +"." );
     }
 }
