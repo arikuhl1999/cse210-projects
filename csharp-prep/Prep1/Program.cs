bbf842fd9e2905b8477ca1972aa14dd636810e59
arikuhl1999 <arielakuhl1999@byui.edu>
2024-01-16 13:32:50 -0700

    added the new ways or ways to add to a file

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,13 +6,16 @@ class Program
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello Prep1 World!");
-        Console.WriteLine("What is your first name? ");
+        Console.Write("What is your first name? ");
         string firstname = Console.ReadLine();
 
-        Console.WriteLine("What is your last name? ");
+        Console.Write("What is your last name? ");
         string lastname = Console.ReadLine();
         
-        
-        System.Console.WriteLine("Your name is "+ lastname + ", " + firstname + " " + lastname +"." );
+        // string concation
+        Console.WriteLine("Your name is "+ lastname + ", " + firstname + " " + lastname +"." );
+
+        // string interpolation
+        Console.WriteLine($"your name is {lastname}, {lastname} {firstname}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -7,3 +8,30 @@ class Program
         Console.WriteLine("Hello Prep2 World!");
     }
 }
+
+// example of if statement in C# is: 
+
+// always put the {curly braces} on there own line
+
+// if (x > y)
+// {
+//    Console.writeline("greater than");
+// }
+
+// if (x > y)
+// {
+//     if (x > z)
+//     {
+//         Console.WriteLine("greater than both");
+//     }
+// }
+
+// if (X > y)
+// {
+//     Console.WriteLine("greater than");
+// }
+// else
+// {
+//     Console.WriteLine("Less than");
+// }
+
