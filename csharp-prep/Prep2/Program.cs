314f7d332c4c3107e8a090a5ee1a01ee2b2bc31b
arikuhl1999 <arielakuhl1999@byui.edu>
2024-01-18 13:27:18 -0700

    Added the code to make it a little bit more presentable and like the example

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,7 +5,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        
+        string answer = Console.ReadLine();
+
+        int percentage = int.Parse(answer);
+
+        string gradeLetter = "";
+
+        if (percentage >= 90)
+        {
+            gradeLetter = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            gradeLetter = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            gradeLetter = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+           gradeLetter = "D";
+        }
+
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"You have the grade of: {gradeLetter}");
+        Console.WriteLine();
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You are doing great, don't lose sight of your goal. You got this.");
+            Console.WriteLine();
+        }
+
+        else 
+        {
+            Console.WriteLine("Keep trying, you can do it.");
+            Console.WriteLine();
+        }
     }
 }
 
@@ -33,5 +79,25 @@ class Program
 // else
 // {
 //     Console.WriteLine("Less than");
+// }
+
+// In python it adds elif but in C# it is else if
+
+// In C#, the and operator is && the or operator is || and the not operator is ! which can be combined together with other expressions and parentheses.
+//  examples would be:
+
+// if (name == "Peter" || name == "James" || name == "John")
+// {
+//     Console.WriteLine("This is a biblical name.");
+// }
+
+// if (firstName == "Brigham" && lastName == "Young")
+// {
+//     Console.WriteLine("Welcome Brother Brigham!");
+// }
+
+// if (!(name == "Peter" || name == "James" || name == "John"))
+// {
+//     Console.WriteLine("This is a not one of those three");
 // }
 
