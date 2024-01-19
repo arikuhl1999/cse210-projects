1e5523ab13b841bcda99e67e3c7bc1bf165c04d1
arikuhl1999 <arielakuhl1999@byui.edu>
2024-01-19 15:53:59 -0700

    did the magic number assignment by comparing it to my cse 110 class

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the Guess my number game: ");
+        Console.WriteLine();
+
+        Random randomObject = new Random();
+        // 
+
+        int magicNumber = randomObject.Next(1,100);
+
+        int userGuess = 0;
+
+        int count = 0;
+
+        while (userGuess != magicNumber)
+        {
+        Console.WriteLine("What is your guess? ");
+        userGuess = int.Parse(Console.ReadLine());
+        // that line is taking what the user said and turning it into an integer
+        count += 1;
+
+        if (userGuess < magicNumber)
+        {
+            System.Console.WriteLine("Higher \n");
+        }
+
+        else if (userGuess > magicNumber)
+        {
+            System.Console.WriteLine("Lower\n");
+        }
+
+        else
+        {
+            System.Console.WriteLine("Awesome sauce, you guessed it.\n");
+        }
+        }
+
+        System.Console.WriteLine($"It took you {count} guesses.");
+        System.Console.WriteLine();
     }
 }
