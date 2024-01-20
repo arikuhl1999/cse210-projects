5312dde5c21074ea242c29e21242b478841037ad
arikuhl1999 <arielakuhl1999@byui.edu>
2024-01-20 09:01:30 -0700

    added some comments from the reading

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -11,4 +11,15 @@ class Program
 /*
  voids help return functions without using the word return. It is empty it doesn't give you anything back.
 
+ return type FunctionName(dataType parameter1, dataType parameter2)
+{
+    //function body
+}
+
+ void DisplayMessage()
+ {
+    console.writeline ("Hello World!");
+ }
+
+
  */
